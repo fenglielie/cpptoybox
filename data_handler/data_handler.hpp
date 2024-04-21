0886ae5d241b8428c88ac7825aa4d6dfac61026c
fenglielie <fenglielie@qq.com>
2024-04-21 14:28:07 +0800

    add data_handler

---
+++ data_handler/data_handler.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#if __cplusplus >= 202002L
+#include <format>
+#define HAS_FORMAT_SUPPORT
+#endif
+
+class FileRAII {
+public:
+    FileRAII(const char *file_name, std::ios::openmode mode)
+        : m_file(file_name, mode) {
+        if (m_file.fail()) {
+            throw std::runtime_error("FileRAII: Failed to open file "
+                                     + std::string(file_name));
+        }
+    }
+
+    FileRAII(const FileRAII &) = delete;
+    FileRAII &operator=(const FileRAII &) = delete;
+
+    FileRAII(FileRAII &&other) noexcept : m_file(std::move(other.m_file)) {}
+
+    FileRAII &operator=(FileRAII &&other) noexcept {
+        m_file = std::move(other.m_file);
+        return *this;
+    }
+
+    ~FileRAII() {
+        if (m_file.is_open()) { m_file.close(); }
+    }
+
+    std::fstream &get_stream() { return m_file; }
+
+private:
+    std::fstream m_file;
+};
+
+template <typename... Ts>
+class DataHandler {
+public:
+    using LineType = std::tuple<Ts...>;
+
+    static std::vector<LineType> read(std::istream &input_stream,
+                                      char delimiter) {
+        std::vector<LineType> result;
+        std::string line;
+        while (std::getline(input_stream, line)) {
+            clean_line(line);
+            if (!line.empty()) { result.push_back(read_line(line, delimiter)); }
+        }
+        return result;
+    }
+
+    static void write(std::ostream &output_stream,
+                      const std::vector<LineType> &data, char delimiter) {
+        for (const auto &line : data) {
+            write_line(output_stream, line, delimiter);
+        }
+    }
+
+private:
+    static void clean_line(std::string &line) {
+        // 替换异常字符为空格
+        std::replace(line.begin(), line.end(), '\r', ' ');
+        std::replace(line.begin(), line.end(), '\t', ' ');
+
+        // 删除行首行末的空格片段
+        line.erase(0, line.find_first_not_of(' '));
+        line.erase(line.find_last_not_of(' ') + 1);
+
+        // 查找注释符#所在位置 并且删除后续的片段
+        std::size_t n_comment_start = line.find_first_of('#');
+        // 定位并删除行尾的注释
+        if (n_comment_start != std::string::npos) {
+            line.erase(n_comment_start);
+        }
+    }
+
+    static LineType read_line(const std::string &str, char delimiter) {
+        std::istringstream ss(str);
+        LineType dataLine;
+        read_tuple_from_stream(ss, dataLine, delimiter);
+        return dataLine;
+    }
+
+    template <std::size_t I = 0, typename... Args>
+    static void read_tuple_from_stream(std::istringstream &ss,
+                                       std::tuple<Args...> &t, char delimiter) {
+        if constexpr (I < sizeof...(Args)) {
+            // 读取数据并检查错误
+            if (!(ss >> std::get<I>(t))) {
+                throw std::runtime_error("Error reading data");
+            }
+
+            // 检查分隔符
+            if constexpr (I < sizeof...(Args) - 1) {
+                if (delimiter != ' ') {
+                    char sep = 0;
+                    if (!(ss >> sep) || sep != delimiter) {
+                        throw std::runtime_error("Delimiter not found");
+                    }
+                }
+            }
+
+            // 递归处理下一个元素
+            read_tuple_from_stream<I + 1, Args...>(ss, t, delimiter);
+        }
+    }
+
+    static void write_line(std::ostream &os, const LineType &dataLine,
+                           char delimiter) {
+        write_tuple_to_stream(os, dataLine, delimiter);
+    }
+
+    template <std::size_t I = 0, typename... Args>
+    static void write_tuple_to_stream(std::ostream &os,
+                                      const std::tuple<Args...> &t,
+                                      char delimiter) {
+        if constexpr (I < sizeof...(Args)) {
+            // 写入数据
+#ifdef HAS_FORMAT_SUPPORT
+            os << std::format("{}", std::get<I>(t));
+#else
+            os << std::get<I>(t);
+#endif
+
+            // 写入分隔符（如果是最后一个元素，则换成换行符）
+            if constexpr (I < sizeof...(Args) - 1) { os << delimiter; }
+            else { os << "\n"; }
+
+            // 递归写入下一个元素
+            write_tuple_to_stream<I + 1, Args...>(os, t, delimiter);
+        }
+    }
+};
+
+#ifdef HAS_FORMAT_SUPPORT
+#undef HAS_FORMAT_SUPPORT
+#endif
---
+++ data_handler/data_handler_doc.md
@@ -0,0 +1,80 @@
+# 数据文件读写
+
+数据文件读写组件`DataHandler`提供如下的静态方法接口：
+
+1. `read` 逐行读取数据
+2. `write` 逐行写入数据
+
+两个接口的定义和主要实现如下
+```cpp
+template <typename... Ts>
+class DataHandler {
+public:
+    using LineType = std::tuple<Ts...>;
+
+    static std::vector<LineType> read(std::istream &input_stream,
+                                      char delimiter) {
+        std::vector<LineType> result;
+        std::string line;
+        while (std::getline(input_stream, line)) {
+            clean_line(line);
+            if (!line.empty()) { result.push_back(read_line(line, delimiter)); }
+        }
+        return result;
+    }
+
+    static void write(std::ostream &output_stream,
+                      const std::vector<LineType> &data, char delimiter) {
+        for (const auto &line : data) {
+            write_line(output_stream, line, delimiter);
+        }
+    }
+}
+```
+
+它们的处理逻辑核心是将每一行数据和一个`std::tuple`相对应，例如一行数据个数如下
+```
+1,10,2.0
+```
+可以对应为
+```cpp
+using LineType = std::tuple<int,int,double>;
+```
+
+- `read`方法需要提供一个输入流（通常是文件输入流），还需要提供行内的分隔符（例如`,`或` `），返回`std::vector<LineType>`数据。
+- `write`方法需要提供一个输出流（通常是文件输出流），提供`std::vector<LineType>`数据，
+也需要提供行内的分隔符（例如`,`或` `），无返回值。
+
+上述接口都需要一个文件流，这里同时提供了针对文件流的RAII封装`FileRAII`，只需要提供文件名和打开方式，省略了打开和关闭文件的操作，在文件打开失败时会抛出异常。
+
+一些细节的处理：
+
+- `read`如果出现无法处理的格式错误（例如没有在数据之间读取到分隔符），会抛异常，不过在行内的最后一个元素之后，无论有没有分隔符，都不会影响`read`的读取；
+- `read`支持`#`开头的注释，包括整行注释和行尾注释，同时`read`不支持`#`作为分隔符；
+- `read`会自动跳过空行，自动将`\t`，`\r`替换为空格；
+- `write`在写入时，不会在行内最后一个元素之后加上分隔符；
+- `read`始终采用`>> item`从输入流中获取数据，`write`对于C++20之前采用`<< item`向输出流传递数据，对于C++20则采用`<< std::format("{}",item)`。
+
+
+使用示例：
+读取一个以`,`作为分隔符的数据文件，再把数据重新以` `作为分隔符写入新文件中，只需要两条语句
+```cpp
+// 读取 Nodes.txt
+auto nodes = DataHandler<int, double, double>::read(
+    FileRAII(PREFIX "/Nodes.csv", std::ios::in).get_stream(), ',');
+// 写入 tmp_Nodes.csv
+DataHandler<int, double, double>::write(
+    FileRAII(PREFIX "/tmp_Nodes.csv", std::ios::out).get_stream(), nodes,' ');
+```
+这里原本的Nodes.csv形如
+```
+35, 1.20086, 2.56496
+36, 3.27009, 2.4683
+37, 2.7188, 2.46564
+```
+重新写入的tmp_Nodes.csv形如
+```
+35 1.20086 2.56496
+36 3.27009 2.4683
+37 2.7188 2.46564
+```
