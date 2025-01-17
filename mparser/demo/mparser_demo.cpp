19809f7ecb2f8058b4f5597ce8b88632b734e7d9
fenglielie <fenglielie@qq.com>
2025-01-17 16:16:06 +0800

    test: add more tests

--- mparser/demo/mparser_demo.cpp
+++ mparser/demo/mparser_demo.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
 
     if (!parser.parse(argc, argv)) {
         parser.print_usage();
-        return 0;
+        return 1;
     }
 
     auto rest = parser.get_rest();
