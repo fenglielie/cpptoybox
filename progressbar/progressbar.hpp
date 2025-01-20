819637063136e30ee96fb982893fbe51a6ca87eb
fenglielie <fenglielie@qq.com>
2025-01-20 16:46:55 +0800

    feat: simplify progressbar

--- progressbar/progressbar.hpp
+++ progressbar/progressbar.hpp
@@ -9,13 +9,8 @@
 class ProgressBar {
 public:
     explicit ProgressBar(double arg_end)
-        : m_arg_end(arg_end), m_start_time(std::chrono::steady_clock::now()) {
-        if (m_arg_end <= 0) {
-            throw std::invalid_argument("arg_end must >= 0");
-        }
-
-        m_last_time = m_start_time;
-    }
+        : m_arg_end(arg_end), m_start_time(std::chrono::steady_clock::now()),
+          m_last_time(m_start_time) {}
 
     void update(double arg_now) {
         data_update(arg_now);
@@ -24,66 +19,42 @@ public:
     }
 
     void finished() {
-        data_update(m_arg_end);
+        update(m_arg_end);
 
-        std::cout << std::format("\n Finished! Cost {:.2f}s\n", time_cost());
+        std::cout << std::format(" Cost {:.2f}s\n", time_cost());
     }
 
     double time_cost() const {
         auto time_now = std::chrono::steady_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
             time_now - m_start_time);
-        return static_cast<double>(duration.count()) / 1000.0;
+        return static_cast<double>(duration.count()) / 1000.0;  // in seconds
     }
 
 private:
-    using TimePoint = std::chrono::steady_clock::time_point;
-
     static constexpr int block_num = 20;
     static constexpr char fill_char = '#';
     static constexpr double alpha = 0.4;  // Smoothing factor
 
     const double m_arg_end;
-    const TimePoint m_start_time;
+    const std::chrono::steady_clock::time_point m_start_time;
 
-    TimePoint m_last_time;
+    std::chrono::steady_clock::time_point m_last_time;
     double m_last_arg = 0;
     double m_last_rate = 0.0;
 
     bool m_initialized = false;
 
     std::string generate_str() const {
-        // Calculate progress percentage and blocks
         double pct = (m_last_arg / m_arg_end);
-        int filled_num = static_cast<int>(pct * block_num);
+        double eta_time = (m_arg_end - m_last_arg) / m_last_rate;
 
-        std::string color_code = calculate_color(pct);
-
-        // Construct progress bar
-        std::string bar_labels;
-        bar_labels.reserve(block_num);
-        bar_labels.append(filled_num, fill_char);
-
-        if (filled_num < block_num) {
-            int last_char = static_cast<int>(pct * 100) % 10;
-            bar_labels.push_back(std::to_string(last_char)[0]);
-            bar_labels.append(block_num - filled_num - 1, ' ');
-        }
-
-        // Prepare the output
-        std::string output =
-            std::format(" [{:6.2f}%][{}]", pct * 100, bar_labels);
-
-        auto predicted_time = (m_arg_end - m_last_arg) / m_last_rate;
-        output += std::format(" [{:.2f}->{:.2f}] {} ", m_last_arg, m_arg_end,
-                              format_prediction_time(predicted_time));
-
-        return color_code + output + "\033[0m";
+        return std::format(" {}[{:6.2f}%][{}][{:.2f}->{:.2f}]{} \033[0m",
+                           color_label(pct), pct * 100, pct_bar(pct),
+                           m_last_arg, m_arg_end, time_str(eta_time));
     }
 
     void data_update(double arg_now) {
-        if (arg_now < 0 || arg_now > m_arg_end) { return; }  // out of range
-
         auto time_now = std::chrono::steady_clock::now();
 
         // Time prediction
@@ -105,12 +76,12 @@ private:
             m_last_rate = alpha * cur_rate + (1 - alpha) * m_last_rate;
         }
 
-        // update
+        // Update
         m_last_time = time_now;
         m_last_arg = arg_now;
     }
 
-    static std::string calculate_color(double pct) {
+    static std::string color_label(double pct) {
         int r = 0;
         int g = 0;
         int b = 0;
@@ -129,11 +100,10 @@ private:
         }
 
         // Return the ANSI escape code for 24-bit RGB color
-        return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";"
-               + std::to_string(b) + "m";
+        return std::format("\033[38;2;{};{};{}m", r, g, b);
     }
 
-    static std::string format_prediction_time(double predicted_time) {
+    static std::string time_str(double predicted_time) {
         if (predicted_time < 1200) {  // less than 20 minutes
             return std::format("{:6.2f}s", predicted_time);
         }
@@ -142,5 +112,22 @@ private:
         }
         // more than 2 hours
         return std::format("{:6.2f}h", predicted_time / 3600);
+    }
+
+    static std::string pct_bar(double pct) {
+        int filled_num = static_cast<int>(pct * block_num);
+
+        // Construct progress bar
+        std::string pct_bar;
+        pct_bar.reserve(block_num);
+        pct_bar.append(filled_num, fill_char);
+
+        if (filled_num < block_num) {
+            int last_char = static_cast<int>(pct * 100) % 10;
+            pct_bar.push_back(std::to_string(last_char)[0]);
+            pct_bar.append(block_num - filled_num - 1, ' ');
+        }
+
+        return pct_bar;
     }
 };
